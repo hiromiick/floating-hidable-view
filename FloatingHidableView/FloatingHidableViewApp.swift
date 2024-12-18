91fd178810a3feb1cb935ba4df6e10295366d798
hiromiick <batterydandan@gmail.com>
2024-12-18 13:06:53 +0900

    Initial Commit

---
+++ FloatingHidableView/FloatingHidableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingHidableViewApp.swift
+//  FloatingHidableView
+//
+//  Created by hiromiick on 2024/12/18.
+//
+
+import SwiftUI
+
+@main
+struct FloatingHidableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
